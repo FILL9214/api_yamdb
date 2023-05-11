1a7c94d37f3a4cbe352ce9c5128caf665defd898
Илья Лифарь <mania-3d@yandex.ru>
2023-05-11 17:49:46 +0300

    базовая модель юзера и админка для нее

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    email = models.EmailField(
+        'e-mail адрес',
+        max_length=100,
+        unique=True,
+        blank=False,
+    )
+    bio = models.TextField(
+        'Биография',
+        blank=True,
+    )
