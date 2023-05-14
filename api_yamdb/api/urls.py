98c0a9ec94a6c4783a75adde6d710054c4852c8b
ajuia-m <user23and@gmail.com>
2023-05-14 12:11:23 +0300

    Исправлены юрлы

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -11,7 +11,6 @@ router = routers.DefaultRouter()
 router.register(r'categories', CategoryViewSet, basename='categories'),
 router.register(r'genres', GenreViewSet, basename='genres'),
 router.register(r'titles', TitleViewSet, basename='titles')
-router = routers.DefaultRouter()
 router.register(
     r'titles/(?P<title_id>\d+)/reviews',
     ReviewViewSet, basename='reviews')
