ae7046f8cabf7dd9c502da96ce322ae1ca0bf6f4
ajuia-m <user23and@gmail.com>
2023-05-19 21:39:05 +0300

    исправлено согласно замечаниям

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 from reviews.models import (
     Category,
@@ -84,24 +85,24 @@ class CommentSerializer(serializers.ModelSerializer):
 class ReviewSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(slug_field='username',
                                           read_only=True)
-    title = serializers.SlugRelatedField(
-        slug_field='name',
-        read_only=True
-    )
 
     class Meta:
         fields = '__all__'
         model = Review
 
-    def create(self, validated_data):
-        if Review.objects.filter(
-            author=self.context['request'].user,
-            title=validated_data.get('title')
-        ).exists():
+    def validate(self, data):
+        request = self.context['request']
+        author = request.user
+        title_id = self.context.get('view').kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
+        if (
+            request.method == 'POST'
+            and Review.objects.filter(title=title, author=author).exists()
+        ):
             raise serializers.ValidationError(
-                'Нельзя оставить больше одного обзора.')
-        review = Review.objects.create(**validated_data,)
-        return review
+                'Нельзя оставить больше одного обзора.'
+                )
+        return data
 
 
 class AdminModerSerializer(serializers.ModelSerializer):
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -76,7 +76,8 @@ class CommentViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         review = get_object_or_404(
             Review,
-            id=self.kwargs.get('review_id'))
+            id=self.kwargs.get('review_id'),
+            title_id=self.kwargs.get('title_id'),)
         return review.comments.all()
 
     def perform_create(self, serializer):
